20c06f1b50a7edd5ca38fc021bf225ef8ee26141
Fantasy Phoenix <fantasyinferno@gmail.com>
2018-07-30 13:30:01 +0900

    Added graph spectral filtering library

---
+++ io_utils.py
@@ -0,0 +1,27 @@
+from os import listdir, makedirs
+from os.path import isfile, join, dirname
+from datetime import datetime
+import pandas as pd
+
+kanto_data_path = "/home/infernov/Desktop/Summer/data/tokyo"
+resampled_data_path = "./resampled"
+file_names = [f for f in listdir(kanto_data_path) if isfile(join(kanto_data_path,f))] # list all files
+
+def parser(x):
+    if x[-4:] == '2400':
+        x = x[:-4] + '0000'
+    return datetime.strptime(x, "%Y%m%d%H%M")
+                             
+def load_sensor_data(name):
+    df = pd.read_csv(join(kanto_data_path, name), delimiter='\t', parse_dates=[0], date_parser=parser, index_col=0)
+    pd.set_index(df, in_place=True)
+    return df
+
+def write_sensor_data(sensor_dataframe, destination):
+    makedirs(dirname(destination), exist_ok=True)
+    sensor_dataframe.to_csv(destination)
+
+def load_resampled_sensor_data(name):
+    df = pd.read_csv(join(resampled_data_path, name))
+    df.set_index(pd.DatetimeIndex(df['datetime']), inplace=True)
+    return df
