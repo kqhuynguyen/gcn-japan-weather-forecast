83a87fbf21719ec0a23818c323a61997b6832323
Huy Nguyen <huynguyen@Huys-iMac.local>
2018-08-09 17:54:17 +0900

    Added gconvnet

---
+++ preprocess_amd.py
@@ -0,0 +1,47 @@
+import numpy as np
+import pandas as pd
+import os
+from datetime import datetime
+
+# parameters for the time series
+periods = 276048 # number of periods is the same in each of the sensor's data
+locations = 1252 # number of sensor locations
+start_date = datetime(2012, 1, 1, 0, 10) # the starting date for the data collection
+freq = "10T" # the frequency of the collection. In this case it is per 10 minutes
+# aids = pd.read_csv("datasets/japan/amd_master.tsv", delimiter="\t", usecols=["aid"]).loc[:,"aid"] # the ids of the sensors
+datapath = "datasets/japan"
+forecast_feature = "max_tp" # the feature we want to forecast
+train_val_split = 0.6 # point at which we split the dataset into train and val
+val_test_split = 0.8 # point at which we split the dataset into val and test
+train_fname = os.path.join("datasets/japan", "amd_{}_train.csv".format(forecast_feature)) # the train output file path
+test_fname = os.path.join("datasets/japan", "amd_{}_test.csv".format(forecast_feature)) # the test output file path
+val_fname = os.path.join("datasets/japan", "amd_{}_val.csv".format(forecast_feature)) # the validation output file path
+sampling_rate = "H" # sampling rate, for resampling, thus reducing the size of the data
+
+# load the data
+all_data_list = []
+for i in range(5):
+    current_amd_path = os.path.join("datasets/japan", "amd{}".format(i + 1))
+    files_in_current_folder = os.listdir(current_amd_path) 
+    for file_name in files_in_current_folder:
+        print("Currently reading: {}".format(file_name))
+        aid = file_name.split("_")[1].split(".")[0] # get the current aid
+        all_data_list.append(pd.read_csv(os.path.join(current_amd_path, file_name), delimiter="\t", usecols=[forecast_feature]).rename(columns={forecast_feature: aid})) # append the dataframe into list
+print("Concatenating the dataframes...")
+output_dataframe = pd.concat(all_data_list, axis=1) # concat the dataframes in the list into a new dataframe
+output_dataframe.set_index(pd.date_range(start=start_date, periods=periods, freq=freq), inplace=True) # set a DatetimeIndex for the dataframe
+print("Dealing with N/A's...")
+output_dataframe.dropna(axis=1, thresh=250000, inplace=True) # only keep sensors with at least 250000 non-NA values
+output_dataframe.fillna(method="bfill", axis=1, inplace=True) # fill the NA values with back fill
+print("Resampling the data...")
+output_dataframe = output_dataframe.resample(sampling_rate).mean() # resample the data
+
+print("Splitting the data...")
+total_length = len(output_dataframe)
+train, val, test = np.split(output_dataframe, [int(.6 * total_length), int(.8 * total_length)]) # split the data into train, validation and test sets
+# dealing with NA values
+print("Saving the data...")
+train.to_csv(train_fname, index_label="datetime")
+test.to_csv(test_fname, index_label="datetime")
+val.to_csv(val_fname, index_label="datetime")
+print("Successfully preprocessed.")
---
+++ utils.py
@@ -0,0 +1,178 @@
+import tensorflow as tf
+import numpy as np
+import pandas as pd
+import math
+import pickle
+import os
+import json
+from datetime import datetime
+from IPython import embed
+import tensorflow.contrib.slim as slim
+from scipy.sparse import coo_matrix
+
+def save_config(model_dir, config):
+    '''
+    save config params in a form of param.json in model directory
+    '''
+    param_path = os.path.join(model_dir, "params.json")
+
+    print("[*] PARAM path: %s" %param_path)
+
+    with open(param_path, 'w') as fp:
+        json.dump(config.__dict__, fp, indent=4, sort_keys=True)
+
+def get_time():
+    return datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+
+def show_all_variables():
+  model_vars = tf.trainable_variables()
+  slim.model_analyzer.analyze_vars(model_vars, print_info=True)
+
+def prepare_dirs(config):
+    if config.load_path:
+        config.model_name = "{}_{}".format(config.task, config.load_path)
+    else:
+        config.model_name = "{}_{}".format(config.task, get_time())
+
+    config.model_dir = os.path.join(config.log_dir, config.model_name)
+
+    for path in [config.model_dir]:
+        if not os.path.exists(path):
+            os.makedirs(path)
+            print("Directory '%s' created" %path)
+            
+
+def pklLoad(fname):
+    with open(fname, 'rb') as f:
+        return pickle.load(f)
+    
+def pklSave(fname, obj):
+    with open(fname, 'wb') as f:
+        pickle.dump(obj, f)
+        
+def convert_to_one_hot(a, max_val=None):
+    N = a.size
+    data = np.ones(N,dtype=int)
+    sparse_out = coo_matrix((data,(np.arange(N),a.ravel())), shape=(N,max_val))
+    return np.array(sparse_out.todense())
+
+# compute the euclidean distance between x and y
+def get_distance(x, y):
+    return np.sqrt(np.sum(np.square(np.subtract(x,y))))
+
+class BatchLoader(object):
+    def __init__(self, data_dir, dataset_name, batch_size, seq_length):
+        sensor_locations_fname = os.path.join(data_dir, dataset_name, 'amd_master.tsv')
+        train_fname = os.path.join(data_dir, dataset_name, "amd_max_tp_train.csv")# the train output file path
+        test_fname = os.path.join(data_dir, dataset_name, "amd_max_tp_test.csv") # the test output file path
+        val_fname = os.path.join(data_dir, dataset_name, "amd_max_tp_val.csv") # the validation output file path
+
+        # get the sensor locations
+        sensor_locations = pd.read_csv(sensor_locations_fname, delimiter="\t")
+        num_sensors = sensor_locations.shape[0]
+
+        # construct an adjancency matrix out of the sensors' locations
+        adj = np.zeros([num_sensors, num_sensors])
+        for i in range(num_sensors):
+            for j in range(num_sensors):
+                x = sensor_locations.iloc[i,:]
+                y = sensor_locations.iloc[j,:]
+                adj[i][j] = np.sqrt((x["lat1"] - y["lat1"])**2 + (x["lng1"] - y["lng1"])**2)
+                adj[j][i] = adj[i][j]
+        
+        train = pd.read_csv(train_fname, index_col="datetime", parse_dates=["datetime"])
+        test = pd.read_csv(test_fname, index_col="datetime", parse_dates=["datetime"])
+        val = pd.read_csv(val_fname, index_col="datetime", parse_dates=["datetime"])
+
+        data = [train, val, test]
+
+        self.sizes = []
+        self.all_batches = []
+        self.all_data = data
+        self.adj = adj
+
+        print("Reshaping tensors...")
+        for split, data in enumerate(data):  # split = 0:train, 1:valid, 2:test
+            #Cutting training sample for check profile fast..(Temporal)
+            #if split==0:
+            #    #Only for training set
+            #    length = data.shape[0]
+            #    data = data[:int(length/4)]
+            
+            length = data.shape[0]
+            data = data[: batch_size * seq_length * int(math.floor(length / (batch_size * seq_length)))]
+            ydata = np.zeros_like(data)
+            ydata[:-1] = data.iloc[1:].copy()
+            ydata[-1] = data[0].copy()
+
+            if split < 2:
+                x_batches = list(data.reshape([-1, batch_size, seq_length]))
+                y_batches = list(ydata.reshape([-1, batch_size, seq_length]))
+                self.sizes.append(len(x_batches))
+            else:
+                x_batches = list(data.reshape([-1, batch_size, seq_length]))
+                y_batches = list(ydata.reshape([-1, batch_size, seq_length]))
+                self.sizes.append(len(x_batches))
+
+            self.all_batches.append([x_batches, y_batches])
+
+        self.batch_idx = [0, 0, 0]
+        print("data load done. Number of batches in train: %d, val: %d, test: %d" \
+              % (self.sizes[0], self.sizes[1], self.sizes[2]))
+
+    def next_batch(self, split_idx):
+        # cycle around to beginning
+        if self.batch_idx[split_idx] >= self.sizes[split_idx]:
+            self.batch_idx[split_idx] = 0
+        idx = self.batch_idx[split_idx]
+        self.batch_idx[split_idx] = self.batch_idx[split_idx] + 1
+        return self.all_batches[split_idx][0][idx], \
+               self.all_batches[split_idx][1][idx]
+
+    def reset_batch_pointer(self, split_idx, batch_idx=None):
+        if batch_idx == None:
+            batch_idx = 0
+        self.batch_idx[split_idx] = batch_idx
+
+    def text_to_tensor(self, input_files, vocab_fname, tensor_fname, Adj_fname):
+        counts = []
+        char2idx = {}
+        idx2char = []
+
+        output = []
+
+        for input_file in input_files:
+            count = 0
+            output_chars = []
+            with open(input_file) as f:
+                for line in f:
+                    line = ''.join(line.split())
+                    chars_in_line = list(line)
+                    chars_in_line.append('|')
+                    for char in chars_in_line:
+                        if char not in char2idx:
+                            idx2char.append(char)
+                            #                 print("idx: %d, char: %s" %(len(idx2char), char))
+                            char2idx[char] = len(idx2char) - 1
+                        output_chars.append(char2idx[char])
+                        count += 1
+            counts.append(count)
+            output.append(np.array(output_chars))
+
+        train_data = output[0]
+        train_data_shift = np.zeros_like(train_data)
+        train_data_shift[:-1] = train_data[1:].copy()
+        train_data_shift[-1] = train_data[0].copy()
+
+        # Co-occurance
+        Adj = np.zeros([len(idx2char), len(idx2char)])
+        for x, y in zip(train_data, train_data_shift):
+            Adj[x, y] += 1
+
+        # Make Adj symmetric & visualize it
+
+
+        print("Number of chars : train %d, val %d, test %d" % (counts[0], counts[1], counts[2]))
+        pklSave(vocab_fname, [idx2char, char2idx])
+        pklSave(tensor_fname, output)
+        pklSave(Adj_fname, Adj)
